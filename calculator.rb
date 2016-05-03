c053db193e23cdf2ac60511c0af81c5d2746e18f
bengus101 <bgustafson29@yahoo.com>
2016-05-02 19:47:40 -0700

    Calculator code done. Simple, but it should work.

---
+++ calculator.rb
@@ -0,0 +1,18 @@
+class Calculator
+	def add(number_1, number_2)
+		number_1 + number_2
+	end
+
+	def subtract(number_1, number_2)
+		number_1 - number_2
+	end
+
+	def divide(number_1, number_2)
+		number_1 / number_2
+	end
+
+	def multiply(number_1, number_2)
+		number_1 * number_2
+	end
+end
+
